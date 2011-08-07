5b976c26d2fbf9cd67f5ea09915dc5720d050fc7
Milan Patel <patelmilan.p@gmail.com>
2011-08-08 02:52:20 +0530

    updated -metaprogramming

---
+++ metaprogramming/checked_attributes/checked_attributes.rb
@@ -0,0 +1,45 @@
+require 'test/unit'
+require 'ruby-debug'
+#debugger
+
+module CheckedAttributes
+  def self.included(base)
+    base.extend(self)
+  end
+
+  def attr_checked(attribute, &validation)
+    define_method "#{attribute}=" do |value|
+      raise 'Invalid attribute' unless validation.call(value)
+      instance_variable_set("@#{attribute}", value)
+    end
+
+    define_method attribute do
+      instance_variable_get "@#{attribute}"
+    end
+  end
+end
+
+class Person
+  include CheckedAttributes
+
+  attr_checked :age do |v|
+    v >= 18
+  end
+end
+
+class TestCheckedAttribute < Test::Unit::TestCase
+  def setup
+    @bob = Person.new
+  end
+
+  def test_accepts_valid_values
+    @bob.age = 20
+    assert_equal 20, @bob.age
+  end
+
+  def test_refuses_invalid_values
+    assert_raises RuntimeError, 'Invalid attribute' do
+      @bob.age = 17
+    end
+  end
+end
