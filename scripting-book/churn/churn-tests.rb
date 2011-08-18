e0cd5bb7dd30c1485fea583850a4b6e9389f660e
Milan Patel <patelmilan.p@gmail.com>
2011-08-19 05:15:10 +0530

    update churn

--- scripting-book/churn/churn-tests.rb
+++ scripting-book/churn/churn-tests.rb
@@ -9,12 +9,18 @@ class ChurnTests < Test::Unit::TestCase
   end
 
   def test_header_format
-    assert_equal("Changes since 2005-08-05:", header(month_before(Time.local(2005, 9, 2))))
+    head = header(svn_date(month_before(Time.now)))
+    assert_equal("Changes between 2011-07-22 and 2011-08-19:", head)
   end
 
   def test_normal_subsystem_line_format
-    assert_equal('         audit ********* (45)',
+    assert_equal('audit          (45 changes)   *********',
                  subsystem_line("audit", 45))
+  end
+
+  def test_no_changes_subsystem_line_format
+    assert_equal('persistence    -              -',
+                 subsystem_line("persistence", 0))
   end
 
   def test_asterisks_for_divide_by_five
@@ -24,6 +30,29 @@ class ChurnTests < Test::Unit::TestCase
   def test_asterisks_for_rounds_up_and_down
     assert_equal('****', asterisks_for(18))
     assert_equal('***', asterisks_for(17))
+    assert_equal('*', asterisks_for(1))
+    assert_equal('*', asterisks_for(2))
+  end
+
+  def test_subversion_log_can_have_no_changes
+    assert_equal(0, extract_change_count_from("------------------------------------------------------------------------\n"))
+  end
+
+  def test_subversion_log_with_changes
+    assert_equal(2, extract_change_count_from("------------------------------------------------------------------------
+r2 | marick | 2005-08-07 14:26:21 -0500 (Mon, 07 Aug 2005) | 1 line
+
+added code to handle merger
+------------------------------------------------------------------------
+r1 | marick | 2005-08-07 14:21:47 -0500 (Mon, 07 Aug 2005) | 1 line
+
+first touches
+No commit for revision 0.
+------------------------------------------------------------------------\n"))
+  end
+
+  def test_svn_date
+    assert_equal('2005-03-04', svn_date(Time.local(2005, 3, 4)))
   end
 
 end
--- scripting-book/churn/churn.rb
+++ scripting-book/churn/churn.rb
@@ -2,25 +2,51 @@ def month_before(a_time)
   a_time - 28 * 24 * 60 * 60
 end
 
-def header(a_time)
-  a_time.strftime("Changes since %Y-%m-%d:")
+def header(an_svn_date)
+  "Changes between #{an_svn_date} and #{svn_date(Time.now)}:"
 end
 
 def subsystem_line(subsystem_name, change_count)
+  return "#{subsystem_name.ljust(14)} #{'-'.ljust(14)} -" if change_count == 0
+
   asterisks = asterisks_for(change_count)
-  "#{subsystem_name.rjust(14)} #{asterisks} (#{change_count})"
+  "#{subsystem_name.ljust(14)} #{"(#{change_count} changes)".ljust(14)} #{asterisks}"
 end
 
 def asterisks_for(an_integer)
+  return '*' if an_integer == 1 or an_integer == 2
   '*' * (an_integer / 5.0).round
+end
+
+def change_count_for(name, start_date)
+  extract_change_count_from(svn_log(name, start_date))
+end
+
+def extract_change_count_from(log_text)
+  lines = log_text.split("\n")
+  dashed_lines = lines.find_all do | line |
+    line.include?('-----')
+  end
+  dashed_lines.length - 1
+end
+
+def svn_log(subsystem, start_date)
+  timespan = "--revision 'HEAD:{#{start_date}}'"
+  root = "svn://rubyforge.org//var/svn/churn-demo"
+
+  `svn log #{timespan} #{root}/#{subsystem}`
+end
+
+def svn_date(a_time)
+  a_time.strftime("%Y-%m-%d")
 end
 
 if $0 == __FILE__
   subsystem_names = ['audit', 'fulfillment', 'persistence', 'ui', 'util', 'inventory']
-  start_date = month_before(Time.now)
+  start_date = svn_date(month_before(Time.now))
 
   puts header(start_date)
   subsystem_names.each do | name |
-    puts subsystem_line(name, change_count_for(name))
+    puts subsystem_line(name, change_count_for(name, start_date))
   end
 end
